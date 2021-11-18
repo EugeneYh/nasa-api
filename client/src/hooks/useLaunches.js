39bb94735c041213ff8a5c9514976bbd8fa50845
EugeneY <yehanovskiy@gmail.com>
2021-11-18 23:11:41 +0200

    small bugfix

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -51,8 +51,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
   const abortLaunch = useCallback(async (id) => {
     const response = await httpAbortLaunch(id);
 
-    // TODO: Set success based on response.
-    const success = response.ok;
+		const success = response.ok;
     if (success) {
       getLaunches();
       onAbortSound();
